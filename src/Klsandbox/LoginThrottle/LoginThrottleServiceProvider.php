fa303ec3a1f53a4193039c117d09cadaaf9a397d
Ibrahim Abdul Rahim <mrwaim@gmail.com>
2016-03-17 12:53:22 +0800

    init

---
+++ src/Klsandbox/LoginThrottle/LoginThrottleServiceProvider.php
@@ -0,0 +1,34 @@
+<?php namespace Klsandbox\LoginThrottle;
+
+use Illuminate\Support\ServiceProvider;
+
+class LoginThrottleServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return [];
+	}
+
+}
