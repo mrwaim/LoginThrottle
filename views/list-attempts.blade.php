274d690cbd8fd31c2420cc74a9dd783ed407bc75
Ibrahim Abdul Rahim <mrwaim@gmail.com>
2016-03-30 15:58:26 +0800

    header

--- views/list-attempts.blade.php
+++ views/list-attempts.blade.php
@@ -1,21 +1,7 @@
 @extends('app')
 
 @section('page-header')
-    <h2>Failed login attempts</h2>
-
-    <div class="right-wrapper pull-right">
-        <ol class="breadcrumbs">
-            <li>
-                <a href="index.html">
-                    <i class="fa fa-home"></i>
-                </a>
-            </li>
-            <li><span>Failed Login Attempts</span></li>
-            <li><span>View All Failed Attempts</span></li>
-        </ol>
-
-        <div class="sidebar-right-toggle"></div>
-    </div>
+    @include('elements.page-header', ['section_title' => 'Failed login attempts', 'page_title' => 'View All Failed Attempts'])
 @endsection
 
 
